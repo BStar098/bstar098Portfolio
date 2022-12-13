6ebffc8a0ddc63a25f2aa63c6aa00b2cec509e12
Bstar098 <luceropintos@abc.gob.ar>
2022-12-13 15:14:14 -0300

    social media anchor tags, hrefs added

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -9,18 +9,34 @@ function SocialMedia() {
         <a
           className="socialMediaIcon"
           href="https://www.linkedin.com/in/santiago-del-sagrado-corazon-pintos-lucero-525b5323b/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <FaLinkedin size={70} />
         </a>
 
-        <a className="socialMediaIcon" href="https://github.com/BStar098">
+        <a
+          className="socialMediaIcon"
+          href="https://github.com/BStar098"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaGithubSquare size={70} />
         </a>
 
-        <a className="socialMediaIcon" href="/">
+        <a
+          className="socialMediaIcon"
+          href="mailto:santinahuel098@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaEnvelope size={70} />
         </a>
-        <a href="/">
+        <a
+          href="https://drive.google.com/uc?export=download&id=1t6eh9hvYaoaxPQ3Bb7joRQz4SFh_HkM-"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <h1 className="cv">CV</h1>
         </a>
       </div>
