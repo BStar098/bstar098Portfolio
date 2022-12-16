bc747bca75faa3ed8eaca6fed88f7dc7b7a21c12
Bstar098 <luceropintos@abc.gob.ar>
2022-12-16 18:42:46 -0300

    first version of my projects done

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Landing from "./components/Landing";
 import AboutMe from "./components/AboutMe";
 import SocialMedia from "./components/SocialMedia";
 import MyProjects from "./components/MyProjects";
+import ScrollingSeparator from "./components/ScrollingSeparator";
 
 function App() {
   return (
     <>
-        <div id="home" className="landingPageContainer">
-          <Navbar />
-          <Landing />
-        </div>
+      <div id="home" className="landingPageContainer">
+        <Navbar />
+        <Landing />
+      </div>
+      <ScrollingSeparator backgroundColor="red" title="ABOUT ME" />
       <AboutMe />
       <SocialMedia />
+      <ScrollingSeparator backgroundColor="lime" title="MY PROJECTS" />
       <MyProjects />
     </>
   );
--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -1,11 +1,66 @@
+import "../styles/MyProjects/myProjects.css";
 import React from "react";
-import ScrollingSeparator from "./ScrollingSeparator";
-import '../styles/myProjects/myProjects.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+import eCommerce from "../assets/myProjectsPictures/E-commerce.png";
+import TMDB from "../assets/myProjectsPictures/Movie_Database.png";
+import portfolio from "../assets/myProjectsPictures/Portfolio.png";
 
 function MyProjects() {
   return (
-    <div id='projects' className="myProjectsContainer">
-      <ScrollingSeparator backgroundColor='lime' title='MY PROJECTS'/>
+    <div id="projects" className="myProjectsContainer">
+      <h1 className="myProjectsTitle">Click the pictures ;)</h1>
+      <div className="swiperContainer">
+        <div className="swiper-wrapper">
+          <Swiper
+            loop={true}
+            slidesPerView={1}
+            navigation={true}
+            modules={[Navigation]}
+          >
+            <SwiperSlide className="swiperPicture">
+              <a
+                href="https://github.com/BStar098/al_lio_front_repo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img alt="alLioEcommerce" src={eCommerce}></img>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="swiperPicture">
+              <a
+                href="https://github.com/BStar098/26-checkpoint-TMDB"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img alt="TMDBClone" src={TMDB}></img>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="swiperPicture">
+              <a
+                href="https://github.com/BStar098/bstar098Portfolio"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img alt="myPortfolio" src={portfolio}></img>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="swiperPicture">
+              <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/odtPaT5Qf0I"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 }
