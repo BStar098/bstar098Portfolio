6bb7d1b3344350f01590984f84944d2c5e4e3e0e
Bstar098 <luceropintos@abc.gob.ar>
2022-12-19 11:17:14 -0300

    myProjects screen dynamic title

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -1,5 +1,5 @@
 import "../styles/MyProjects/myProjects.css";
-import React from "react";
+import { React, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -7,15 +7,21 @@ import { Navigation } from "swiper";
 import eCommerce from "../assets/myProjectsPictures/E-commerce.png";
 import TMDB from "../assets/myProjectsPictures/Movie_Database.png";
 import portfolio from "../assets/myProjectsPictures/Portfolio.png";
-
 import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+import Button from "./Button";
 
 function MyProjects() {
+  const [projectTitle, setProjectTitle] = useState("");
   return (
     <div id="projects" className="myProjectsContainer">
-      <h1 className="myProjectsTitle">Click the pictures ;)</h1>
-
       <div className="swiperContainer">
+        <Button
+          className="projectsTitle"
+          size="3vw"
+          background="lime"
+          color="white"
+          text={projectTitle ? projectTitle : "Al lío E-commerce"}
+        />
         <div className="button-prev-slide">
           <GoArrowLeft className="carouselNavigationIcon" />
         </div>
@@ -24,6 +30,9 @@ function MyProjects() {
         </div>
         <div className="swiper-wrapper">
           <Swiper
+            onSlideChange={(e) => {
+              setProjectTitle(e.slides[e.snapIndex].id);
+            }}
             loop={true}
             slidesPerView={1}
             navigation={{
@@ -32,43 +41,81 @@ function MyProjects() {
             }}
             modules={[Navigation]}
           >
-            <SwiperSlide className="swiperPicture">
-              <a
-                href="https://github.com/BStar098/al_lio_front_repo"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img alt="alLioEcommerce" src={eCommerce}></img>
-              </a>
+            <SwiperSlide id="Al lío E-commerce" className="swiperPicture">
+              <img alt="alLioEcommerce" src={eCommerce}></img>
+              <div className="swiperButtonsContainer">
+                <Button
+                  href="https://github.com/BStar098/al_lio_front_repo"
+                  className="swiperButtons"
+                  size="3vw"
+                  background="lime"
+                  color="white"
+                  text="Front repo."
+                />
+                <Button
+                  href="https://github.com/BStar098/al_lio_back_repo"
+                  className="swiperButtons"
+                  size="3vw"
+                  background="lime"
+                  color="white"
+                  text="Back repo."
+                />
+              </div>
             </SwiperSlide>
-            <SwiperSlide className="swiperPicture">
-              <a
-                href="https://github.com/BStar098/26-checkpoint-TMDB"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img alt="TMDBClone" src={TMDB}></img>
-              </a>
+            <SwiperSlide id="TMDB Clone" className="swiperPicture">
+              <img alt="TMDBClone" src={TMDB}></img>
+              <div className="swiperButtonsContainer">
+                <Button
+                  href="https://github.com/BStar098/26-checkpoint-TMDB"
+                  className="swiperButtons"
+                  size="3vw"
+                  background="lime"
+                  color="white"
+                  text="Main Repo."
+                />
+              </div>
             </SwiperSlide>
-            <SwiperSlide className="swiperPicture">
-              <a
-                href="https://github.com/BStar098/bstar098Portfolio"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img alt="myPortfolio" src={portfolio}></img>
-              </a>
+            <SwiperSlide id="My Brutalist Portfolio" className="swiperPicture">
+              <img alt="myPortfolio" src={portfolio}></img>
+              <div className="swiperButtonsContainer">
+                <Button
+                  href="https://github.com/BStar098/bstar098Portfolio"
+                  className="swiperButtons"
+                  size="3vw"
+                  background="lime"
+                  color="white"
+                  text="Main repo."
+                />
+              </div>
             </SwiperSlide>
-            <SwiperSlide className="swiperPicture">
+            <SwiperSlide id="RRHH Mobile App" className="swiperPicture">
               <iframe
                 width="100%"
-                height="100%"
+                height="90%"
                 src="https://www.youtube.com/embed/odtPaT5Qf0I"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
               ></iframe>
+              <div className="swiperButtonsContainer">
+                <Button
+                  href="https://github.com/BStar098/FRONT-App-RRHH"
+                  className="swiperButtons"
+                  size="3vw"
+                  background="lime"
+                  color="white"
+                  text="Front repo."
+                />
+                <Button
+                  href="https://github.com/BStar098/BACK-App-RRHH"
+                  className="swiperButtons"
+                  size="3vw"
+                  background="lime"
+                  color="white"
+                  text="Back repo."
+                />
+              </div>
             </SwiperSlide>
           </Swiper>
         </div>
